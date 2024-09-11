498869c89aac7285c43aeeb8dc8f2f91bd6c66c7
fengwanyan <fengwy1@chinatelecom.cn>
2024-09-11 10:17:39 +0800

    框架搭建

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,44 +1,7 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text color-red text-30>{{ title }}</text>
-    </view>
-  </view>
+  <view> 首页 </view>
 </template>
 
-<script setup lang="ts">
-let title = $ref("Hello");
+<script setup lang="ts"></script>
 
-onMounted(() => {
-  title = "优研平台";
-});
-</script>
-
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
-</style>
+<style></style>
---
+++ src/pages/study/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <view> 学习 </view>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
